03a2b05133f37493d1c7489dbc1ca238f26f7ee1
Arthur Jorge <arthur.sjorge@gmail.com>
2026-02-06 09:27:23 +0100

    lab done

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,22 @@
-// src/main.ts
+
+import { getLocation, getCurrentWeather, displayLocation, displayWeatherData, updateBackground } from './utils';
+
+const form = document.getElementById('weather-form') as HTMLFormElement;
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    
+    const input = document.getElementById('location-input') as HTMLInputElement;
+    const locationName = input.value;
+    
+    getLocation(locationName)
+        .then((locationResponse) => {
+            const locationDetails = locationResponse.results[0];
+            displayLocation(locationDetails);
+            return getCurrentWeather(locationDetails);
+        })
+        .then((weatherData) => {
+            displayWeatherData(weatherData);
+            updateBackground(weatherData.current_weather.weathercode, weatherData.current_weather.is_day);
+        });
+});
--- src/utils.ts
+++ src/utils.ts
@@ -1,14 +1,60 @@
-// src/utils.ts
-
 import axios from 'axios';
-import { LocationResponse, Location } from "./types";
-
-
+import { LocationResponse, Location, WeatherResponse } from './types';
 
 export function getLocation(locationName: string): Promise<LocationResponse> {
     const url = `https://geocoding-api.open-meteo.com/v1/search?name=${locationName}&count=1`;
     return axios.get(url).then((response) => response.data);
 }
 
+export function getCurrentWeather(locationDetails: Location): Promise<WeatherResponse> {
+    const url = `https://api.open-meteo.com/v1/forecast?latitude=${locationDetails.latitude}&longitude=${locationDetails.longitude}&current_weather=true&models=icon_global`;
+    return axios.get(url).then((response) => response.data);
+}
 
+export function displayLocation(locationDetails: Location): void {
+    const locationNameElm = document.getElementById('location-name') as HTMLElement;
+    locationNameElm.innerText = locationDetails.name;
 
+    const countryElm = document.getElementById('country') as HTMLElement;
+    countryElm.innerText = "(" + locationDetails.country + ")";
+}
+
+export function displayWeatherData(obj: WeatherResponse): void {
+    const temperatureElm = document.getElementById('temperature') as HTMLElement;
+    temperatureElm.innerText = `${obj.current_weather.temperature} ${obj.current_weather_units.temperature}`;
+
+    const windspeedElm = document.getElementById('windspeed') as HTMLElement;
+    windspeedElm.innerText = `${obj.current_weather.windspeed} ${obj.current_weather_units.windspeed}`;
+
+    const winddirectionElm = document.getElementById('winddirection') as HTMLElement;
+    winddirectionElm.innerText = `${obj.current_weather.winddirection} °`;
+}
+
+export function updateBackground(weatherCode: number, isDay: number): void {
+    const body = document.querySelector('body') as HTMLElement;
+    const firstChar = weatherCode.toString()[0];
+    
+    let className = '';
+    
+    if (firstChar === '0' || firstChar === '1') {
+        className = isDay === 0 ? 'sunny-night' : 'sunny';
+    } else if (firstChar === '2') {
+        className = isDay === 0 ? 'partly-cloudy-night' : 'partly-cloudy';
+    } else if (firstChar === '3') {
+        className = 'cloudy';
+    } else if (firstChar === '4') {
+        className = 'foggy';
+    } else if (firstChar === '5') {
+        className = 'drizzle';
+    } else if (firstChar === '6') {
+        className = 'rain';
+    } else if (firstChar === '7') {
+        className = 'snow';
+    } else if (firstChar === '8') {
+        className = 'showers';
+    } else if (firstChar === '9') {
+        className = 'thunderstorm';
+    }
+    
+    body.className = className;
+}
